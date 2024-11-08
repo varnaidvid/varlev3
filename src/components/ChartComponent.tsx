34d917a46133e3c5419ea2a99befa91ac18fd35e
ldaniel2726 <lenart.daniel.peter@gmail.com>
2024-11-08 14:20:39 +0100

    Styling ChartComponent

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,18 +1,31 @@
 'use client';
 
 import { LineChart, Line, XAxis, CartesianGrid, Tooltip } from "recharts";
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 interface ChartComponentProps {
   data: { key: string; value: number }[];
 }
 
+const chartConfig = {
+  value: {
+    color: "#000",
+  },
+} satisfies ChartConfig
+
 export default function ChartComponent({ data }: ChartComponentProps) {
   return (
-    <LineChart width={400} height={400} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="key" />
-      <Tooltip />
-      <Line type="monotone" dataKey="value" stroke="#8884d8" />
-    </LineChart>
+    <ChartContainer config={chartConfig}>
+      <LineChart width={400} height={400} data={data} margin={{ left: 12, right: 12 }}>
+        <CartesianGrid vertical={false} />
+        <XAxis dataKey="key" />
+        <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+        <Line type="natural" stroke="#000" dataKey="value" strokeWidth={2} dot={false}/>
+      </LineChart>
+    </ChartContainer>
   );
 }
