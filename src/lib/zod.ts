451803b7e4add2e1099fe3db96a2bc9687b4c9f1
vebesz <barnabas.varszegi@gmail.com>
2024-11-08 13:44:21 +0100

    fixed registration form regex and data-table component

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -19,13 +19,16 @@ export const signUpSchema = z
       .string({ required_error: "Adja meg a jelszavát!" })
       .min(1, "Adja meg a jelszavát!")
       .min(8, "Legalább 8 karakter hosszú jelszó szükséges")
-      .max(32, "Legfeljebb 32 karakter hosszú jelszó engedélyezett"),
+      .max(48, "Legfeljebb 48 karakter hosszú jelszó engedélyezett"),
     password2: z.string(),
     name: z
       .string({ required_error: "Adja meg a nevét!" })
-      .min(1, "Adja meg a nevét!")
-      .max(32, "Legfeljebb 32 karakter hosszú név engedélyezett")
-      .regex(/^(?!.* {3})(?=.* )[a-zA-Z ]{1,32}$/, "Teljes nevét adja meg!"),
+      .min(3, "Adja meg a teljes nevét!")
+      .max(48, "Legfeljebb 48 karakter hosszú név engedélyezett")
+      .regex(
+        /^(?!.* {3})(?=.* )[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]{1,48}$/,
+        "Teljes nevét adja meg!",
+      ),
   })
   .refine((data) => data.password === data.password2, {
     message: "A jelszavak nem egyeznek",
