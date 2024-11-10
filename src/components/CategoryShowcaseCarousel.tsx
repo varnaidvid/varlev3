eceb0587bf9eeabab57a5cbbf743e977b7618f9e
varnaidvid <varnaidavid0522@gmail.com>
2024-11-10 20:08:58 +0100

    -_-

--- src/components/CategoryShowcaseCarousel.tsx
+++ src/components/CategoryShowcaseCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
+import CompetitionCard from "@/app/(landing)/versenyek/competition-card-lp";
 import {
   Carousel,
   CarouselContent,
@@ -8,8 +8,6 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import SignUpModal from "@/components/SignUpModal";
-import CompetitionCard from "@/components/vezerlopult/versenyek/competition-card";
 import { CompetitionWithDetails } from "@/server/api/routers/competition";
 
 export function CategoryShowcaseCarousel({
@@ -21,12 +19,10 @@ export function CategoryShowcaseCarousel({
     <div className="m-auto mx-20 my-2">
       <Carousel className="w-full p-6">
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
+          {competitions.map((competition, index) => (
             <CarouselItem className="md:basis-1/2 lg:basis-1/3" key={index}>
               <div className="p-1">
-                {competitions[0] && (
-                  <CompetitionCard competition={competitions[0]} />
-                )}
+                <CompetitionCard competition={competition} />
               </div>
             </CarouselItem>
           ))}
